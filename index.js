db44975f6547d0748dc50ae0cce08534531d00ec
veronikavanivska <veronikavanivska2005@gmail.com>
2024-11-28 08:58:28 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+// define endpoint for exercise 1 here
+app.get('/math/circle/:r', (req, res) => {
+//TODO1  
+  res.json(result);
+});
+
+//TODO2
+
+
+//TODO3
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
